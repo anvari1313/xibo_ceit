2c603b35ad40cf8add1e5c5f701110329a8ccff9
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-02 14:12:27 +0330

    Adding first cooperation with cms

--- integration/views.py
+++ integration/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from xibo.requests import get_time
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'integration/index.html', {'time': '22:03 GMT'})
+
+
+def sync(request):
+    r = get_time()
+
+    return render(request, 'integration/index.html',  r)
