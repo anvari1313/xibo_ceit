1ea5b7acdc1d5c8d1dd50457c582314745ee7751
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-13 23:04:41 +0330

    Adding route for display index

--- integration/urls.py
+++ integration/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .views import Display
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name="integration/index.html")),
+    url(r'display/$', Display.index, name='index'),
     url(r'sync/$', views.sync, name='sync')
 ]
--- integration/views.py
+++ integration/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from xibo.requests import get_time
+from xibo.requests import XiboRest
 
 
 def index(request):
-    return render(request, 'integration/index.html', {'time': '22:03 GMT'})
+    return render(request, 'integration/home.html', {'time': '22:03 GMT'})
+
+
+class Display:
+    @staticmethod
+    def index(request):
+        r = XiboRest.get_all_displays()
+        print(r)
+        return render(request, 'integration/display/index.html', {'displays': r})
 
 
 def sync(request):
-    r = get_time()
+    r = XiboRest.get_time()
 
     return render(request, 'integration/index.html',  r)
