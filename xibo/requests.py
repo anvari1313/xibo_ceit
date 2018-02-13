97802544055f9e2a84154448bbe584199d359452
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-13 23:03:31 +0330

    Adding authorize for requests

--- xibo/requests.py
+++ xibo/requests.py
@@ -1,22 +1,29 @@
 import requests
+import dotenv
+from .exception import CanNotAuthorize
 
+xibo_cms_domain = 'http://xibo.ceit.aut.ac.ir'
 
-xibo_cms_domain = 'http://sinage.ceit.aut.ac.ir'
+client_id = dotenv.get('CLIENT_ID')
 
-client_id = 'T6qH5Kth9pXasnlAg9WOYCeYH9w7lUpqc5EM3cIx'
+# client_secret = 'jaTZ09YERMvs2Ecf9WEsJtxXrW0g71WMvc' \
+#                 'ISvqM4gMAkDVDPzE4NE72AzKSFZ4HoAu3p' \
+#                 'Rw3NrO3k1B1aKlwyVvSmLEIpXKTKRQg1u2' \
+#                 'k19dkKYfjPBLVZHZkeIV36oJQurqEJ2TEd' \
+#                 'UV3yJ3mndzuK66qI4oO1qJ2MQMKgaxjJk9' \
+#                 'NjEhszUvZmPhch3qGpWwOIEkf835DvwQxy' \
+#                 '042ucItGf3CB19jPLjygWMwwYcAMnpGkQL' \
+#                 'rhcxUZQ6PuUX1IA2'
 
-client_secret = 'jaTZ09YERMvs2Ecf9WEsJtxXrW0g71WMvc' \
-                'ISvqM4gMAkDVDPzE4NE72AzKSFZ4HoAu3p' \
-                'Rw3NrO3k1B1aKlwyVvSmLEIpXKTKRQg1u2' \
-                'k19dkKYfjPBLVZHZkeIV36oJQurqEJ2TEd' \
-                'UV3yJ3mndzuK66qI4oO1qJ2MQMKgaxjJk9' \
-                'NjEhszUvZmPhch3qGpWwOIEkf835DvwQxy' \
-                '042ucItGf3CB19jPLjygWMwwYcAMnpGkQL' \
-                'rhcxUZQ6PuUX1IA2'
+client_secret = dotenv.get('CLIENT_SECRET')
 
 api_routes = {
     'authenticate': '/api/authorize/access_token',
-    'clock': '/api/clock'
+    'clock': '/api/clock',
+    # 'display': {
+    #     'all': '/api/display'
+    # }
+    'display': '/api/display'
 }
 
 
@@ -29,9 +36,47 @@ def authenticate():
     return {'Authorization': auth['token_type'] + ' ' + auth['access_token']}
 
 
-def get_time():
-    headers = authenticate()
-    url = xibo_cms_domain + api_routes['clock']
-    r = requests.get(url=url, headers=headers)
+# def get_time():
+#     headers = authenticate()
+#     url = xibo_cms_domain + api_routes['clock']
+#     r = requests.get(url=url, headers=headers)
+#     print(dotenv.get('CLIENT_ID'))
+#     return r.json()
 
-    return r.json()
+
+class XiboRest:
+    request_headers = {}
+
+    @staticmethod
+    def __authenticate():
+        url = xibo_cms_domain + api_routes['authenticate']
+        payload = {'client_id': client_id, 'client_secret': client_secret, 'grant_type': 'client_credentials'}
+        r = requests.post(url=url, data=payload)
+        print('Authenticating')
+        auth = r.json()
+        XiboRest.request_headers['Authorization'] = auth['token_type'] + ' ' + auth['access_token']
+
+    @staticmethod
+    def get_time():
+        print('Getting time')
+        url = xibo_cms_domain + api_routes['clock']
+        r = requests.get(url=url, headers=XiboRest.request_headers)
+        if r.status_code / 100 != 2:
+            XiboRest.__authenticate()
+            r = requests.get(url=url, headers=XiboRest.request_headers)
+            if r.status_code == 401:
+                raise CanNotAuthorize
+
+        print(r.status_code)
+        return r.json()
+
+    @staticmethod
+    def get_all_displays():
+        url = xibo_cms_domain + api_routes['display']
+        r = requests.get(url=url, headers=XiboRest.request_headers)
+        if r.status_code / 100 != 2:
+            XiboRest.__authenticate()
+            r = requests.get(url=url, headers=XiboRest.request_headers)
+
+        print(r.status_code)
+        return r.json()
