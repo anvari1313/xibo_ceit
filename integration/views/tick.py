a9081ea28d6e512064752f6cad3ca7947fee7b63
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-20 23:20:36 +0330

    Adding tick view

--- integration/views/tick.py
+++ integration/views/tick.py
@@ -1,6 +1,13 @@
 from django.http import JsonResponse
 import time
+from xibo.requests import XiboRest
 
 
 def tick(request):
-    return JsonResponse({'NOW': time.time()})
+    str = '<p style=\"text-align: center;\"><font color=\"#ffffff\"><span style=\"fon' \
+          't-size: 48px;\">شبکه های غیر کامپیوتری</span></font></p>\r\n\r\n<p style=\"' \
+          'text-align: center;\"><font color=\"#ffffff\"><span style=\"font-size: 48px;' \
+          '\">دکتر صبایی</span></font></p>\r\n'
+
+    result = XiboRest.update_widget(widget_id=7, text=str)
+    return JsonResponse({'NOW': time.time(), 'res': result})
