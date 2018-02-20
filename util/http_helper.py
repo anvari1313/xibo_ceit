e7e6fa1f7e4d11ed5f11e0574448fbba935fe120
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-20 20:07:12 +0330

    Adding helper function

---
+++ util/http_helper.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponseNotAllowed
+from django.utils.cache import logger
+
+def method_dispatch(**table):
+    def invalid_method(request, *args, **kwargs):
+        logger.warning('Method Not Allowed (%s): %s', request.method, request.path,
+            extra= {
+                'status_code': 405,
+                'request': request
+            }
+        )
+        return HttpResponseNotAllowed(table.keys())
+
+    def d(request, *args, **kwargs):
+        handler = table.get(request.method, invalid_method)
+        return handler(request, *args, **kwargs)
+    return d
