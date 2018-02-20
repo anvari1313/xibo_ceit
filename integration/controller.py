0a97bafbfc0de64db8a18132f1d0cd5f90371638
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-20 12:03:46 +0330

    Changing the file name for views

--- integration/controller.py
+++ integration/controller.py
@@ -8,16 +8,26 @@ def index(request):
     return render(request, 'integration/home.html', {'time': '22:03 GMT'})
 
 
+class Xibo:
+    @staticmethod
+    def index(request):
+        layouts = XiboRest.get_all_layouts()
+        print(layouts)
+        displays = XiboRest.get_all_displays()
+        print(displays)
+        return render(request, 'integration/xibo/index.html', {'layouts': layouts, 'displays': displays})
+
+
 class Display:
     @staticmethod
     def index(request):
         r = XiboRest.get_all_displays()
         print(r)
-        return render(request, 'integration/display/index.html', {'displays': r})
+        return render(request, 'integration/xibo/display/index.html', {'displays': r})
 
 
 def sync(request):
-    task1(12)
+    task1(12, repeat=1)
     r = XiboRest.get_time()
 
     return render(request, 'integration/index.html',  r)
--- integration/urls.py
+++ integration/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from . import views
-from .views import Display
+from . import controller
+from .controller import Display
+from .views.xibo import Xibo
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="integration/index.html")),
+    url(r'xibo/$', Xibo.index, name='xibo.index'),
     url(r'display/$', Display.index, name='index'),
-    url(r'sync/$', views.sync, name='sync')
+    url(r'sync/$', controller.sync, name='sync')
 ]
