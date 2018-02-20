af3a8e7f25d03a7848f0623cbe1b423fdefb28ee
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-20 20:09:35 +0330

    Adding url for login

--- integration/urls.py
+++ integration/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import url
+
+
 from . import controller
 from .controller import Display
 from .views.xibo import Xibo
+from .views.user import login_user
+from .views.home import Home
 from django.views.generic import TemplateView
+from util.http_helper import method_dispatch
+
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="integration/index.html")),
+    url(r'^$', Home.index),
     url(r'xibo/$', Xibo.index, name='xibo.index'),
     url(r'display/$', Display.index, name='index'),
+    url(r'user/login/$', method_dispatch(
+        POST=login_user,
+        GET=TemplateView.as_view(template_name="integration/user/login.html")), name='user.login'),
     url(r'sync/$', controller.sync, name='sync')
 ]
