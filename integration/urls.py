7be9ba86c7f6143ab13aa5a1cc5e081d1c3441aa
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-21 15:53:21 +0330

    Adding urls for updating

--- integration/urls.py
+++ integration/urls.py
@@ -7,6 +7,7 @@ from .views.xibo import Xibo
 from .views.user import login_user
 from .views.home import Home
 from .views.tick import tick
+from .views.task import Task, TaskListView
 from django.views.generic import TemplateView
 from util.http_helper import method_dispatch
 
@@ -14,9 +15,18 @@ from util.http_helper import method_dispatch
 urlpatterns = [
     url(r'^$', Home.index),
     url(r'xibo/$', Xibo.index, name='xibo.index'),
+    url(r'xibo/update/widget$', method_dispatch(GET=Xibo.update_widget), name='xibo.update.widget'),
+    url(r'xibo/update/layout$', method_dispatch(GET=Xibo.update_layout), name='xibo.update.layout'),
+    url(r'xibo/update/display', method_dispatch(GET=Xibo.update_display), name='xibo.update.display'),
+
     url(r'display/$', Display.index, name='index'),
     url(r'user/login/$', method_dispatch(
         POST=login_user,
         GET=TemplateView.as_view(template_name="integration/user/login.html")), name='user.login'),
-    url(r'tick/$', method_dispatch(GET=tick), name='sync')
+    url(r'tick/$', method_dispatch(GET=tick), name='tick'),
+    url(r'task/$', method_dispatch(GET=TaskListView.as_view()), name='task.index'),
+    url(r'task/new/$', method_dispatch(
+        GET=TemplateView.as_view(template_name="integration/task/schedule.html"),
+        POST=Task.new
+    ), name='task.new')
 ]
