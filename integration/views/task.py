504eee23d3e073a4e6ff5e6ef0263e4ad64121b0
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-21 17:14:12 +0330

    Adding task with model

--- integration/views/task.py
+++ integration/views/task.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from integration.models import Task
+from integration.models import TaskSchedule, Widget
 import datetime
 
 
 class TaskListView(generic.ListView):
-    model = Task
+    model = TaskSchedule
     template_name = 'integration/task/index.html'
 
 
 class Task:
     @staticmethod
     def new(request):
-        widget_id = request.POST['widget-id']
-        _datetime = request.POST['datetime']
+        widget = Widget.objects.get(widget_id=int(request.POST['widget-id']))
+        hour = int(request.POST['hour'])
+        minute = int(request.POST['minute'])
         text = request.POST['text']
-        task_obj = Task()
-        task_obj.widget_id = widget_id
-        task_obj.text = text
-        task_obj.task_datetime = datetime.datetime.now()
-        task_obj.save()
+        task_obj = TaskSchedule(widget=widget, text=text, task_datetime_hour=hour, task_datetime_min=minute)
+
+        r = task_obj.save()
+
         return redirect('/task')
