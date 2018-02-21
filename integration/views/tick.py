eb09be4999d77039098bd11c1f0b7008bfd52e4b
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-21 11:33:54 +0330

    Changing the text

--- integration/views/tick.py
+++ integration/views/tick.py
@@ -1,13 +1,16 @@
 from django.http import JsonResponse
 import time
 from xibo.requests import XiboRest
-
+from integration.models import Task
 
 def tick(request):
     str = '<p style=\"text-align: center;\"><font color=\"#ffffff\"><span style=\"fon' \
-          't-size: 48px;\">شبکه های غیر کامپیوتری</span></font></p>\r\n\r\n<p style=\"' \
+          't-size: 48px;\">شبکه های کامپیوتری</span></font></p>\r\n\r\n<p style=\"' \
           'text-align: center;\"><font color=\"#ffffff\"><span style=\"font-size: 48px;' \
           '\">دکتر صبایی</span></font></p>\r\n'
 
     result = XiboRest.update_widget(widget_id=7, text=str)
+
+    # Task.objects.filter(date)
+
     return JsonResponse({'NOW': time.time(), 'res': result})
