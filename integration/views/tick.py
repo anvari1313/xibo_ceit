bbd290b14a4b7c0c1789637392b65d2408face9f
Ahmad Anvari <ahmad.anv@gmail.com>
2018-02-21 11:48:44 +0330

    Setting the base templates

--- integration/views/tick.py
+++ integration/views/tick.py
@@ -5,7 +5,7 @@ from integration.models import Task
 
 def tick(request):
     str = '<p style=\"text-align: center;\"><font color=\"#ffffff\"><span style=\"fon' \
-          't-size: 48px;\">شبکه های کامپیوتری</span></font></p>\r\n\r\n<p style=\"' \
+          't-size: 48px;\">مدار های الکتریکی دکتر ممتاز پور</span></font></p>\r\n\r\n<p style=\"' \
           'text-align: center;\"><font color=\"#ffffff\"><span style=\"font-size: 48px;' \
           '\">دکتر صبایی</span></font></p>\r\n'
 
