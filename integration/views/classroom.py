fea7e0aff46421ceb7ee013bcc5eb63de3514b9c
Ahmad Anvari <ahmad.anv@gmail.com>
2018-04-25 20:43:38 +0430

    Implement the classroom schedule

--- integration/views/classroom.py
+++ integration/views/classroom.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from integration.models import ClassRoom
 from integration.models import Display
+from integration.models import Widget
 from integration.models import ClassroomSchedule
 from django.views import View
+from django.http import Http404
 
 
 class ClassRoomView(View):
@@ -12,7 +14,30 @@ class ClassRoomView(View):
         displays = Display.objects.all()
         class_room = ClassRoom.objects.all()
 
-        return render(request, 'integration/class/index.html', {'displays': displays, 'class_rooms': class_room})
+        displays = Display.objects.all()
+        teacher_name_widgets = Widget.objects.all()
+        subject_name_widgets = Widget.objects.all()
+        return render(request, 'integration/class/index.html', {
+            'displays': displays,
+            'class_rooms': class_room,
+            'teacher_name_widgets': teacher_name_widgets,
+            'subject_name_widgets': subject_name_widgets
+        })
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('classroom-name')
+        display_id = request.POST.get('display-id')
+        teacher_name_widget_id = request.POST.get('teacher-name-widget')
+        subject_name_widget_id = request.POST.get('subject-name-widget')
+
+        display = Display.objects.get(display_id=display_id)
+        teacher_name_widget = Widget.objects.get(widget_id=teacher_name_widget_id)
+        subject_name_widget = Widget.objects.get(widget_id=subject_name_widget_id)
+
+        classroom = ClassRoom(name=name, display=display,
+                              teacher_name_widget=teacher_name_widget, subject_name_widget=subject_name_widget)
+        classroom.save()
+        return redirect(reverse('classroom.index'))
 
 
 class ClassRoomTable(View):
@@ -30,18 +55,32 @@ class ClassRoomTable(View):
         ]
 
         classroom_id = kwargs.get('classroom_id', "any_default")
-        class_room = ClassRoom.objects.get(id=classroom_id)
+        try:
+            class_room = ClassRoom.objects.get(id=classroom_id)
+        except ClassRoom.DoesNotExist:
+            raise Http404
+
         schedules = ClassroomSchedule.objects.filter(classroom=class_room)
         return render(request, 'integration/class/table.html', {'class_room': class_room, 'schedules': schedules,
                                                                 'week_days': week_days})
 
     def post(self, request, *args, **kwargs):
         classroom_id = kwargs.get('classroom_id', "any_default")
-        class_room = ClassRoom.objects.get(id=classroom_id)
+        try:
+            class_room = ClassRoom.objects.get(id=classroom_id)
+        except ClassRoom.DoesNotExist:
+            raise Http404
+
         start_time_min = request.POST.get('start-time-min')
         start_time_hour = request.POST.get('start-time-hour')
         teacher_name = request.POST.get('teacher-name')
         subject_name = request.POST.get('subject-name')
         week_day = request.POST.get('week-day')
-        classroom_schedule = ClassroomSchedule()
+        classroom_schedule = ClassroomSchedule(start_time_hour=start_time_hour,
+                                               start_time_min=start_time_min,
+                                               teacher_name=teacher_name,
+                                               subject_name=subject_name,
+                                               week_day=week_day,
+                                               classroom=class_room)
+        classroom_schedule.save()
         return redirect(reverse('classroom.class_id', args=[classroom_id]))
