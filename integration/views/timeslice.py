7986d1aaab25cb5f21d51050b7f48117c74923f2
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-05 15:19:08 +0430

    Change

--- integration/views/timeslice.py
+++ integration/views/timeslice.py
@@ -5,13 +5,13 @@ from integration.models import ClassTimeSlice
 
 class ClassTimeSliceView(View):
 
-    def get(self, request, *args, **kwargs):
-        time_slices = ClassTimeSlice.objects.all()
-        return render(request, "integration/timeslice/class.html", {'timeslices': time_slices})
+        def get(self, request, *args, **kwargs):
+            time_slices = ClassTimeSlice.objects.all()
+            return render(request, "integration/timeslice/class.html", {'timeslices': time_slices})
 
-    def post(self, request, *args, **kwargs):
-        s_time = request.POST.get('start-time')
-        e_time = request.POST.get('end-time')
-        time_slice = ClassTimeSlice(start_time='7:45', end_time='9:15')
-        time_slice.save()
-        return redirect('/admin/timeslice/class/')
+        def post(self, request, *args, **kwargs):
+            s_time = request.POST.get('start-time')
+            e_time = request.POST.get('end-time')
+            time_slice = ClassTimeSlice(start_time='7:45', end_time='9:15')
+            time_slice.save()
+            return redirect('/admin/timeslice/class/')
