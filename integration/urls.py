c5e7297485451f57c39ed732f5269b179f4e65e4
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-26 14:54:17 +0430

    Adding three views

--- integration/urls.py
+++ integration/urls.py
@@ -8,10 +8,8 @@ from .views.xibo import Xibo
 from .views.user import login_user
 from .views.home import Home
 from .views.tick import MinuteTicker
-from .views.task import Task, TaskListView
-from .views.timesheet import DisplaySelect, ScheduleDisplay, ScheduleContent
-from .views.timeslice import ClassTimeSliceView
-from .views.classtimesheet import ClassTimeSheet
+from .views.template_text import TemplateTextView
+from .views.thesis import ThesisView
 from django.views.generic import TemplateView
 from .views.classroom import ClassRoomView, ClassRoomTable, ClassRoomTableThesis
 from util.http_helper import method_dispatch
@@ -29,18 +27,11 @@ urlpatterns = [
         POST=login_user,
         GET=TemplateView.as_view(template_name="integration/user/login.html")), name='user.login'),
     url(r'tick/every/minute/$', MinuteTicker.as_view(), name='tick.minute'),
-
-    url(r'task/$', method_dispatch(GET=TaskListView.as_view()), name='task.index'),
-    url(r'task/new/$', method_dispatch(
-        GET=TemplateView.as_view(template_name="integration/task/schedule.html"),
-        POST=Task.new
-    ), name='task.new'),
-    url(r'timesheet/$', DisplaySelect.as_view(), name="timesheet.selectdisplay"),
-    path('timesheet/display/<int:display_id>/', ScheduleDisplay.as_view(), name="timesheet.tabletime"),
-    path('timesheet/display/<int:display_id>/content/', ScheduleContent.as_view(), name="timesheet.content"),
-    path('class/table/', ClassTimeSheet.as_view(), name="somenameforpath"),
-    path('admin/timeslice/class/', ClassTimeSliceView.as_view(), name="timeslice.class"),
     path('classroom/rclass/<int:classroom_id>/', ClassRoomTable.as_view(), name="classroom.class_id"),
     path('classroom/thesis/<int:classroom_id>/', ClassRoomTableThesis.as_view(), name="thesis.class_id"),
-    path('admin/classroom/', ClassRoomView.as_view(), name='classroom.index')
+    path('admin/classroom/', ClassRoomView.as_view(), name='classroom.index'),
+    path('admin/templatetext/', TemplateTextView.as_view(), name='templatetext.index'),
+    path('thesis/', ThesisView.as_view(), name='thesis'),
+    url(r'ad/$', method_dispatch(
+        GET=TemplateView.as_view(template_name="integration/admin.html")), name='user.login'),
 ]
