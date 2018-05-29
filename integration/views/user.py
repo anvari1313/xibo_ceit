3b05d5859e93958973e03809787ddda4feb8c275
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-29 17:44:06 +0430

    Changing the style of views

--- integration/views/user.py
+++ integration/views/user.py
@@ -1,14 +1,27 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.views import View
+from django.urls import reverse
 
 
-def login_user(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    print(user)
-    if user is not None:
-        login(request, user)
-        return redirect('/')
-    else:
-        return render(request, 'integration/user/logout.html')
+class UserLoginView(View):
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'integration/user/login.html')
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'integration/user/login.html')
+
+
+class UserLogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect(reverse("user.login"))
