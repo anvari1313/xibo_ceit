8ad2d79831657b04601227e9663e86ddfb2c3db6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-29 23:38:32 +0430

    Adding view for display alias

--- integration/views/display.py
+++ integration/views/display.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from integration.models import Display
 from django.views import View
+from django.http import Http404
+from django.urls import reverse
 
 
 class DisplayView(View):
@@ -10,3 +12,17 @@ class DisplayView(View):
         return render(request, 'integration/display/index.html', {
             'displays': displays,
         })
+
+
+class DisplayAliasView(View):
+    def post(self, request, *args, **kwargs):
+        display_id = kwargs.get('display_id', "any_default")
+        try:
+            display = Display.objects.get(display_id=display_id)
+        except Display.DoesNotExist:
+            raise Http404
+        alias_name = request.POST.get('alias-name')
+        display.alias = alias_name
+        display.save(update_fields=['alias'])
+
+        return redirect(reverse('display.index'))
