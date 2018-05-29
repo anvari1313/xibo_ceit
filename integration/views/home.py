207e2e23185feed1224fd8d7a9ddabf6fa7bd591
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-29 17:43:33 +0430

    Making home need auth

--- integration/views/home.py
+++ integration/views/home.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 class Home:
     @staticmethod
     def index(request):
-        return render(request, 'integration/index.html', {'user': {'name': 'user1'}})
+        if request.user.is_authenticated:
+            return render(request, 'integration/index.html', {'user': {'name': 'user1'}})
+        else:
+            return redirect(reverse("user.login"))
