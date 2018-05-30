f2345e910e5c8bd80abce754e5e15e52fb30f9e6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-05-30 06:05:39 +0430

    Adding check auth to views

--- integration/views/display.py
+++ integration/views/display.py
@@ -7,22 +7,28 @@ from django.urls import reverse
 
 class DisplayView(View):
     def get(self, request, *args, **kwargs):
-        displays = Display.objects.all()
+        if request.user.is_authenticated:
+            displays = Display.objects.all()
 
-        return render(request, 'integration/display/index.html', {
-            'displays': displays,
-        })
+            return render(request, 'integration/display/index.html', {
+                'displays': displays,
+            })
+        else:
+            return redirect(reverse("user.login"))
 
 
 class DisplayAliasView(View):
     def post(self, request, *args, **kwargs):
-        display_id = kwargs.get('display_id', "any_default")
-        try:
-            display = Display.objects.get(display_id=display_id)
-        except Display.DoesNotExist:
-            raise Http404
-        alias_name = request.POST.get('alias-name')
-        display.alias = alias_name
-        display.save(update_fields=['alias'])
+        if request.user.is_authenticated:
+            display_id = kwargs.get('display_id', "any_default")
+            try:
+                display = Display.objects.get(display_id=display_id)
+            except Display.DoesNotExist:
+                raise Http404
+            alias_name = request.POST.get('alias-name')
+            display.alias = alias_name
+            display.save(update_fields=['alias'])
 
-        return redirect(reverse('display.index'))
+            return redirect(reverse('display.index'))
+        else:
+            return redirect(reverse("user.login"))
