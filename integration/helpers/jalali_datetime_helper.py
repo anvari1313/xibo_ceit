bf760c8f284271ebf6759eb00c95e1b2fd69b210
Ahmad Anvari <ahmad.anv@gmail.com>
2018-08-10 19:58:57 +0430

    Implement jalali datetime helper methods

---
+++ integration/helpers/jalali_datetime_helper.py
@@ -0,0 +1,32 @@
+__jalali_month = [
+    'فروردین',
+    'اردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'آبان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+]
+
+__jalali_weekdays = [
+    'شنبه',
+    'یک شنبه',
+    'دو شنبه',
+    'سه شنبه',
+    'چهار شنبه',
+    'پنج شنبه',
+    'جمعه',
+]
+
+
+def month_name(month_index):
+    return __jalali_month[month_index]
+
+
+def weekday_name(weekday_index):
+    return __jalali_weekdays[weekday_index]
